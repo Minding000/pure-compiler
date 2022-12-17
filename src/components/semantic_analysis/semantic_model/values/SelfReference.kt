4f8f8c7ff8eb1cf3593132d8c30b39570672f99f
Paul Ohlhauser <bendix.ohlhauser@gmail.com>
2022-12-17 22:42:55 +0100

    Fixed self references being allowed outside of type definitions.

--- src/components/semantic_analysis/semantic_model/values/SelfReference.kt
+++ src/components/semantic_analysis/semantic_model/values/SelfReference.kt
@@ -4,15 +4,20 @@ import components.semantic_analysis.Linter
 import components.semantic_analysis.semantic_model.definitions.TypeDefinition
 import components.semantic_analysis.semantic_model.scopes.Scope
 import components.semantic_analysis.semantic_model.types.ObjectType
+import messages.Message
 import components.syntax_parser.syntax_tree.literals.SelfReference as SelfReferenceSyntaxTree
 
 open class SelfReference(override val source: SelfReferenceSyntaxTree): Value(source) {
 	var definition: TypeDefinition? = null
 
 	override fun linkValues(linter: Linter, scope: Scope) {
-		definition = scope.getSurroundingDefinition() //TODO add linter error if self reference is used outside of type definition
-		definition?.let { definition ->
-			type = ObjectType(definition)
+		val surroundingDefinition = scope.getSurroundingDefinition()
+		if(surroundingDefinition == null) {
+			linter.addMessage(source, "Self references are not allowed outside of type definitions.",
+				Message.Type.ERROR)
+		} else {
+			definition = surroundingDefinition
+			type = ObjectType(surroundingDefinition)
 		}
 	}
 }
--- tests/components/semantic_analysis/resolution/SelfReference.kt
+++ tests/components/semantic_analysis/resolution/SelfReference.kt
@@ -2,6 +2,7 @@ package components.semantic_analysis.resolution
 
 import components.semantic_analysis.semantic_model.types.ObjectType
 import components.semantic_analysis.semantic_model.values.SelfReference
+import messages.Message
 import org.junit.jupiter.api.Test
 import util.TestUtil
 import kotlin.test.assertEquals
@@ -10,10 +11,37 @@ import kotlin.test.assertIs
 internal class SelfReference {
 
 	@Test
+	fun `detects self reference keyword outside of type definition`() {
+		val sourceCode =
+			"""
+				this
+            """.trimIndent()
+		val lintResult = TestUtil.lint(sourceCode)
+		lintResult.assertMessageEmitted(Message.Type.ERROR,
+			"Self references are not allowed outside of type definitions")
+	}
+
+	@Test
 	fun `resolves self reference keyword in class`() {
 		val sourceCode =
 			"""
 				Car class {
+					to getCar(): Car {
+						return this
+					}
+				}
+            """.trimIndent()
+		val lintResult = TestUtil.lint(sourceCode)
+		val selfReferenceType = lintResult.find<SelfReference>()?.type
+		assertIs<ObjectType>(selfReferenceType)
+		assertEquals("Car", selfReferenceType.definition?.name)
+	}
+
+	@Test
+	fun `resolves self reference keyword in enum`() {
+		val sourceCode =
+			"""
+				Car enum {
 					to getCar(): Car {
 						return this
 					}
@@ -39,21 +67,5 @@ internal class SelfReference {
 		val selfReferenceType = lintResult.find<SelfReference>()?.type
 		assertIs<ObjectType>(selfReferenceType)
 		assertEquals("FastestCar", selfReferenceType.definition?.name)
-	}
-
-	@Test
-	fun `resolves self reference keyword in enum`() {
-		val sourceCode =
-			"""
-				Car enum {
-					to getCar(): Car {
-						return this
-					}
-				}
-            """.trimIndent()
-		val lintResult = TestUtil.lint(sourceCode)
-		val selfReferenceType = lintResult.find<SelfReference>()?.type
-		assertIs<ObjectType>(selfReferenceType)
-		assertEquals("Car", selfReferenceType.definition?.name)
 	}
 }
