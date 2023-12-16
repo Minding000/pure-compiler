f9407ef1751b27d22022bdb4d6cf8d66905914a0
Paul Ohlhauser <bendix.ohlhauser@gmail.com>
2023-12-16 16:21:55 +0100

    Added LLVM support for specific in self reference.

--- src/components/semantic_model/values/SelfReference.kt
+++ src/components/semantic_model/values/SelfReference.kt
@@ -2,10 +2,12 @@ package components.semantic_model.values
 
 import components.code_generation.llvm.LlvmConstructor
 import components.code_generation.llvm.LlvmValue
+import components.semantic_model.context.Context
 import components.semantic_model.declarations.TypeDeclaration
 import components.semantic_model.scopes.Scope
 import components.semantic_model.types.ObjectType
 import components.semantic_model.types.SelfType
+import errors.internal.CompilerError
 import logger.issues.resolution.SelfReferenceOutsideOfTypeDefinition
 import logger.issues.resolution.SelfReferenceSpecifierNotBound
 import components.syntax_parser.syntax_tree.literals.SelfReference as SelfReferenceSyntaxTree
@@ -37,7 +39,8 @@ open class SelfReference(override val source: SelfReferenceSyntaxTree, scope: Sc
 					context.addIssue(SelfReferenceSpecifierNotBound(source, surroundingDefinition, specifierDefinition))
 			}
 		}
-		typeDeclaration?.let { typeDeclaration ->
+		val typeDeclaration = typeDeclaration
+		if(typeDeclaration != null) {
 			type = SelfType(typeDeclaration)
 			type?.determineTypes()
 			addSemanticModels(type)
@@ -57,6 +60,24 @@ open class SelfReference(override val source: SelfReferenceSyntaxTree, scope: Sc
 	}
 
 	override fun createLlvmValue(constructor: LlvmConstructor): LlvmValue {
-		return context.getThisParameter(constructor)
+		var currentValue = context.getThisParameter(constructor)
+		if(specifier != null) {
+			val specifiedTypeDeclaration = typeDeclaration
+				?: throw CompilerError(source, "Self reference is missing a target type declaration.")
+			var currentTypeDeclaration = scope.getSurroundingTypeDeclaration()
+				?: throw CompilerError(source, "Self reference is missing a surrounding type declaration.")
+			while(currentTypeDeclaration != specifiedTypeDeclaration) {
+				if(!currentTypeDeclaration.isBound)
+					throw CompilerError(source,
+						"Specified type declaration of self reference not found in its surrounding type declaration.")
+				val parentProperty = constructor.buildGetPropertyPointer(currentTypeDeclaration.llvmType, currentValue,
+					Context.PARENT_PROPERTY_INDEX, "_parentProperty")
+				currentValue = constructor.buildLoad(constructor.pointerType, parentProperty, "_parent")
+				currentTypeDeclaration = currentTypeDeclaration.parent?.scope?.getSurroundingTypeDeclaration()
+					?: throw CompilerError(source,
+						"Specified type declaration of self reference not found in its surrounding type declaration.")
+			}
+		}
+		return currentValue
 	}
 }
---
+++ tests/components/code_generation/SelfReference.kt
@@ -0,0 +1,31 @@
+package components.code_generation
+
+import components.code_generation.llvm.Llvm
+import org.junit.jupiter.api.Test
+import util.TestUtil
+import kotlin.test.assertEquals
+
+internal class SelfReference {
+
+	@Test
+	fun `allows targeting bound enclosing class`() {
+		val sourceCode = """
+			Cookie class {
+				to getNewId(): Int {
+					return 38
+				}
+				bound Crumb class {
+					var id = this<Cookie>.getNewId()
+				}
+			}
+			SimplestApp object {
+				to getA(): Int {
+					val cookie = Cookie()
+					return cookie.Crumb().id
+				}
+			}
+			""".trimIndent()
+		val result = TestUtil.run(sourceCode, "Test:SimplestApp.getA")
+		assertEquals(38, Llvm.castToSignedInteger(result))
+	}
+}
