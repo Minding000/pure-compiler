a2ca9985cc60352bdac7ea34cc6ab6e26b23fd20
Paul Ohlhauser <bendix.ohlhauser@gmail.com>
2024-04-02 01:40:31 +0200

    Added tests for value conversion in binary operators.

---
+++ tests/components/code_generation/operators/BinaryPrimitiveOperators.kt
@@ -0,0 +1,48 @@
+package components.code_generation.operators
+
+import components.code_generation.llvm.Llvm
+import org.junit.jupiter.api.Test
+import util.TestUtil
+import kotlin.test.assertEquals
+
+internal class BinaryPrimitiveOperators {
+
+	@Test
+	fun `compiles with primitive generic properties on left hand side`() {
+		val sourceCode = """
+			Container class {
+				containing Item
+				var a: Item
+				init(a)
+			}
+			SimplestApp object {
+				to getTwentyFour(): Int {
+					val container = Container(8)
+					return container.a * 3
+				}
+			}
+			""".trimIndent()
+		val result = TestUtil.run(sourceCode, "Test:SimplestApp.getTwentyFour")
+		assertEquals(24, Llvm.castToSignedInteger(result))
+	}
+
+	@Test
+	fun `compiles with primitive generic properties on right hand side`() {
+		val sourceCode = """
+			Container class {
+				containing Item
+				var a: Item
+				init(a)
+			}
+			SimplestApp object {
+				to getThree(): Int {
+					var b = 9
+					val container = Container(3)
+					return b / container.a
+				}
+			}
+			""".trimIndent()
+		val result = TestUtil.run(sourceCode, "Test:SimplestApp.getThree")
+		assertEquals(3, Llvm.castToSignedInteger(result))
+	}
+}
