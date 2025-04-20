3a1ee9e8268a5f144de992578e4bb07099d0c38b
Paul Ohlhauser <bendix.ohlhauser@gmail.com>
2025-04-20 17:07:10 +0200

    Added native flush implementation for the NativeOutputStream

--- src/components/code_generation/llvm/native_implementations/NativeOutputStreamNatives.kt
+++ src/components/code_generation/llvm/native_implementations/NativeOutputStreamNatives.kt
@@ -10,6 +10,7 @@ class NativeOutputStreamNatives(val context: Context) {
 	fun load(registry: NativeRegistry) {
 		registry.registerNativeImplementation("NativeOutputStream.writeByte(Byte)", ::writeByte)
 		registry.registerNativeImplementation("NativeOutputStream.writeBytes(ByteArray)", ::writeBytes)
+		registry.registerNativeImplementation("NativeOutputStream.flush()", ::flush)
 	}
 
 	private fun writeByte(constructor: LlvmConstructor, llvmFunctionValue: LlvmValue) {
@@ -26,8 +27,6 @@ class NativeOutputStreamNatives(val context: Context) {
 		val byteSize = constructor.buildInt64(1)
 		val byteCount = constructor.buildInt64(1)
 		constructor.buildFunctionCall(context.externalFunctions.streamWrite, listOf(byteVariable, byteSize, byteCount, handle))
-
-		constructor.buildFunctionCall(context.externalFunctions.streamFlush, listOf(constructor.nullPointer))
 
 		constructor.buildReturn()
 	}
@@ -50,6 +49,19 @@ class NativeOutputStreamNatives(val context: Context) {
 		val arrayValue = constructor.buildLoad(constructor.pointerType, arrayValueProperty, "arrayValue")
 		val byteSize = constructor.buildInt64(1)
 		constructor.buildFunctionCall(context.externalFunctions.streamWrite, listOf(arrayValue, byteSize, arraySizeAsLong, handle))
+
+		constructor.buildReturn()
+	}
+
+	private fun flush(constructor: LlvmConstructor, llvmFunctionValue: LlvmValue) {
+		constructor.createAndSelectEntrypointBlock(llvmFunctionValue)
+		val exceptionAddress = context.getExceptionParameter(constructor) //TODO error handling
+		val thisObject = context.getThisParameter(constructor)
+
+		val handleProperty = context.standardLibrary.nativeOutputStream.getNativeValueProperty(constructor, thisObject)
+		val handle = constructor.buildLoad(constructor.pointerType, handleProperty, "handle")
+
+		constructor.buildFunctionCall(context.externalFunctions.streamFlush, listOf(handle))
 
 		constructor.buildReturn()
 	}
