493aafe5d4a7cf834d710d1a7c8f0432a0741dfe
Paul Ohlhauser <bendix.ohlhauser@gmail.com>
2025-04-21 13:32:53 +0200

    Code cleanup

--- tests/components/syntax_parser/FunctionDefinitions.kt
+++ tests/components/syntax_parser/FunctionDefinitions.kt
@@ -3,7 +3,7 @@ package components.syntax_parser
 import org.junit.jupiter.api.Test
 import util.TestUtil
 
-internal class FunctionDefinitionDefinitions {
+internal class FunctionDefinitions {
 
 	@Test
 	fun `parses function definitions`() {
